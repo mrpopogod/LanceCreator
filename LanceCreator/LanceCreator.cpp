8e278447ab0e36d22310939062825a2783ac7d42
Aaron Doukas <amyleeslave@gmail.com>
2019-09-01 13:32:34 -0700

    Fixing bug with explosion not happening

--- LanceCreator/LanceCreator.cpp
+++ LanceCreator/LanceCreator.cpp
@@ -7,6 +7,7 @@
 #include <map>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <set>
 #include <algorithm>
 #include <climits>
@@ -17,11 +18,6 @@ struct Mech {
     string model_name;
     string variant_name;
     int bv;
-
-    bool operator==(const Mech& rhs) const
-    {
-        return model_name == rhs.model_name && variant_name == rhs.variant_name && bv == rhs.bv;
-    }
 };
 
 // name, then variant
@@ -41,11 +37,6 @@ struct Model {
 	int count;
     string name;
 	list<Mech> variants;
-
-    bool operator==(const Model& rhs) const
-    {
-        return count == rhs.count && name == rhs.name && variants == rhs.variants;
-    }
 };
 
 bool compare_model(Model& lhs, Model& rhs)
@@ -55,25 +46,29 @@ bool compare_model(Model& lhs, Model& rhs)
 
 struct ModelForce {
     list<Model> models;
+    string model_string;
 
     void sort()
     {
         models.sort(compare_model);
-    }
+        stringstream ss;
+        for (auto iter = models.begin(); iter != models.end(); iter++)
+        {
+            ss << iter->name;
+        }
 
-    bool operator==(const ModelForce& rhs) const
-    {
-        return (models == rhs.models);
+        model_string = ss.str();
     }
 
     bool operator<(const ModelForce& rhs) const
     {
-        return models.front().name < rhs.models.front().name;
+        return model_string < rhs.model_string;
     }
 };
 
 struct Force {
 	list<Mech> mechs;
+    string mech_string;
     int bv = 0;
 
     void sort()
@@ -84,39 +79,59 @@ struct Force {
     void calculate_bv()
     {
         bv = 0;
+        stringstream ss;
         for (auto iter = mechs.begin(); iter != mechs.end(); iter++)
         {
             bv += iter->bv;
+            ss << iter->model_name << iter->variant_name;
         }
-    }
 
-	bool operator==(const Force& rhs) const
-	{
-		return (mechs == rhs.mechs);
-	}
+        mech_string = ss.str();
+    }
 
     bool operator<(const Force& rhs) const
     {
-        return bv < rhs.bv;
+        return mech_string < rhs.mech_string;
     }
 };
 
 list<Model> models = {
-    { 2, "Commando", { Mech({"Commando", "1B", 616}), Mech({"Commando", "1C", 458}), Mech({"Commando", "1D", 558}), Mech({"Commando", "2D", 541}), Mech({"Commando", "3A", 540}), Mech({"Commando", "5S", 557 }) } },
+    { 1, "Commando", { Mech({"Commando", "1B", 616}), Mech({"Commando", "1C", 458}), Mech({"Commando", "1D", 558}), Mech({"Commando", "2D", 541}), Mech({"Commando", "3A", 540}), Mech({"Commando", "5S", 557 }) } },
     { 2, "Spider", { Mech({"Spider", "5D", 524}), Mech({"Spider", "5K", 503}), Mech({"Spider", "5V", 622}), Mech({"Spider", "7M", 621}) } },
-    { 2, "Jenner", { Mech({"Jenner", "D", 875}), Mech({"Jenner", "F", 1011}), Mech({"Jenner", "K", 889}) } },
+    { 2, "Jenner", { Mech({"Jenner", "D", 875}), Mech({"Jenner", "F", 1011}), Mech({"Jenner", "A", 712}), Mech({"Jenner", "K", 889}) } },
     { 2, "Panther", { Mech({"Panther", "8Z", 741}), Mech({"Panther", "9R", 769}), Mech({"Panther", "10K", 838}) } },
     { 1, "Assassin", { Mech({"Assassin", "21", 749}), Mech({"Assassin", "23", 740}) } },
     { 2, "Cicada", { Mech({"Cicada", "2A", 659}), Mech({"Cicada", "2B", 626}), Mech({"Cicada", "3C", 771}), Mech({"Cicada", "3F", 1329}), Mech({"Cicada", "3M", 812}) } },
-    { 2, "Clint", { Mech({"Clint", "2-3T", 770}), Mech({"Clint", "2-3U", 1081}), Mech({"Clint", "2-4T", 619}) } },
+    { 2, "Clint", { Mech({"Clint", "2-3T", 770}), Mech({"Clint", "2-3U", 1081}) } },
+    { 2, "Hermes II", { Mech({"Heremes II", "2S", 784}), Mech({"Hermes II", "2M", 910}), Mech({"Hermes II", "4K", 976}), Mech({"Hermes II", "5S", 857}) } },
+    { 1, "Whitworth", { Mech({"Whitworth", "1", 981}), Mech({"Whitworth", "1S", 917}), Mech({"Whitworth", "2", 932}) } },
+    { 2, "Vindicator", { Mech({"Vindicator", "1R", 1024}), Mech({"Vindicator", "1SIC", 1020}), Mech({"Vindicator", "3L", 1105}) } },
+    { 1 , "Enforcer", { Mech({"Enforcer", "4R", 1032}), Mech({"Enforcer", "5D", 1308}) } },
+    { 1, "Hunchback", { Mech({"Hunchback", "4G", 1041}), Mech({"Hunchback", "4H", 1062}), Mech({"Hunchback", "4J", 1143}), Mech({"Hunchback", "4N", 1087}), Mech({"Hunchback", "4P", 1138}), Mech({"Hunchback", "4SP", 1043}), Mech({"Hunchback", "5M", 1056}) } },
+    { 2, "Trebuchet", { Mech({"Trebuchet", "3C", 1342}), Mech({"Trebuchet", "5N", 1191}), Mech({"Trebuchet", "5J", 1191}), Mech({"Trebuchet", "5S", 984}), Mech({"Trebuchet", "7M", 1398}) } },
+    { 2, "Dervish", { Mech({"Dervish", "6M", 1146}), Mech({"Dervish", "7D", 1412}) } },
+    { 2, "Dragon", { Mech({"Dragon", "1N", 1125}), Mech({"Dragon", "1C", 1215}), Mech({"Dragon", "5N", 1223}) } },
+    { 2, "Quickdraw", { Mech({"Quickdraw", "4G", 1192}), Mech({"Quickdraw", "4H", 1242}), Mech({"Quickdraw", "5A", 1196}), Mech({"Quickdraw", "5K", 1265}), Mech({"Quickdraw", "5M", 1237}) } },
+    { 2, "Catapult", { Mech({"Catapult", "C1", 1399}), Mech({"Catapult", "A1", 1285}), Mech({"Catapult", "C4", 1358}), Mech({"Catapult", "K2", 1319}), Mech({"Catapult", "C1b", 1570}) } },
+    { 1, "JagerMech", { Mech({"JagerMech", "S", 901}), Mech({"JagerMech", "A", 1122}), Mech({"JagerMech", "DD", 965}) } },
+    { 2, "Grasshopper", { Mech({"Grasshopper", "5H", 1427}), Mech({"Grasshopper", "5N", 1511}), Mech({"Grasshopper", "5J", 1358}) } },
+    { 2, "Awesome", { Mech({"Awesome", "8Q", 1605}), Mech({"Awesome", "8R", 1470}), Mech({"Awesome", "8T", 1593}), Mech({"Awesome", "8V", 1510}), Mech({"Awesome", "9M", 1812}) } },
+    { 2, "Zeus", { Mech({"Zeus", "6S", 1374}), Mech({"Zeus", "5S", 1499}), Mech({"Zeus", "5T", 1729}), Mech({"Zeus", "6T", 1436}), Mech({"Zeus", "9S", 1639}), Mech({"Zeus", "9SS", 1769}) } },\
+    { 2, "Cyclops", { Mech({"Cyclops", "10-Z", 1317}), Mech({"Cyclops", "10-q", 1584}), Mech({"Cyclops", "11-A", 1547}) } },
+    { 2, "Banshee", { Mech({"Banshee", "3E", 1422}), Mech({"Banshee", "3M", 1595}), Mech({"Banshee", "3Q", 1394}), Mech({"Banshee", "3S", 1751}), Mech({"Banshee", "3MC", 1487}) } },
+    { 2, "Atlas", { Mech({"Atlas", "D", 1897}), Mech({"Atlas", "RS", 1849}), Mech({"Atlas", "A", 1787}), Mech({"Atlas", "K", 2175}) } },
+    { 1, "BattleMaster", { Mech({"BattleMaster", "1G", 1519}), Mech({"BattleMaster", "1Gb", 1825}), Mech({"BattleMaster", "1D", 1522}), Mech({"BattleMaster", "1S", 1507}), Mech({"BattleMaster", "3M", 1697}), Mech({"BattleMaster", "3S", 1441}) } },
 
 };
+
+// TODO: something is going wrong and we aren't getting nearly as many lances as we should be; could be something in the cutdown
+// or just a problem with set duplication detection being wrong
 
 // Lance version.  At some point create a Star and L2 version (ideally I would do it in a way that lets me set
 // just a number, but as for now it's harder than I'd like to do so.
 int main()
 {
-    int max_bv = 3000;
+    int max_bv = 5000;
     set<Force> force_set;
     list<Model> models_with_duplicates;
     set<ModelForce> model_force_set;
@@ -130,6 +145,8 @@ int main()
         }
 	}
 
+    // Optimization; calculate min and max BV of each model force and use that to strip out anything grossly
+    // outside the range of what we want; that way we can speed up processing by not exploding so much garbage
     // Create the model forces; each of these would then be exploded into the actual specific variants
     for (auto iter1 = models_with_duplicates.begin(); iter1 != models_with_duplicates.end(); iter1++)
     {
@@ -215,7 +232,6 @@ int main()
     ofstream file;
     file.open("lances.csv");
 
-    file << "Lances small to big" << endl;
     for (auto iter = force_set.begin(); iter != force_set.end(); iter++)
     {
 		for (auto mech_iter = iter->mechs.begin(); mech_iter != iter->mechs.end(); mech_iter++)
