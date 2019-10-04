46506a908da0fe9969adce7eb5e9a7fcbd14433c
Aaron Doukas <amyleeslave@gmail.com>
2019-10-03 20:10:13 -0700

    Command line options

--- LanceCreator/LanceCreator.cpp
+++ LanceCreator/LanceCreator.cpp
@@ -216,6 +216,16 @@ Force build_force(initializer_list<Mech> mechs)
     return force;
 }
 
+void print_usage()
+{
+    cout << "Usage: " << endl
+        << "LanceCreator [--maxBV bv] [--forceSize size] [--numForces num] [--underPercentage percentage]" << endl << endl
+        << "maxBV - The maximum BV a force can have" << endl
+        << "forceSize - The numer of units in a force" << endl
+        << "numForces - The number of compositions of units in a force to generate and run through permutations of variants on" << endl
+        << "underPercentage - The lowest percentage of maxBV the force must be (e.g. 0.95 means a force will have 95% of maxBV or more" << endl;
+}
+
 // Generate a series of potential forces using the configured set of mech models.
 // Supports up to a force size of 6, so can do lances, stars, and L2s.
 // Set the models list to be one of the pregenerated lists
@@ -224,12 +234,119 @@ Force build_force(initializer_list<Mech> mechs)
 // By scoping the number of combinations down we avoid an untenable combinatoric explosion.
 // All forces will be within a particular specified BV, never over, and never under the cutoff (so
 // we don't have to hit the exact BV but don't give up too much as well).
-int main()
+int main(int argc, char** argv)
 {
-    int max_bv = 2400;
-    int force_size = 2;
-    float under_percentage = 0.95f;
-    int num_model_forces = 5;
+    int max_bv = -1;
+    int force_size = -1;
+    int num_model_forces = -1;
+    float under_percentage = -1.0;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--help") == 0)
+        {
+            print_usage();
+            exit(0);
+        }
+        else if (strcmp(argv[i], "--maxBV") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                i++;
+                max_bv = atoi(argv[i]);
+                if (max_bv < 1)
+                {
+                    cout << "Max BV must be a positive number" << endl;
+                    exit(1);
+                }
+            }
+            else
+            {
+                print_usage();
+                exit(1);
+            }
+        }
+        else if (strcmp(argv[i], "--forceSize") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                i++;
+                force_size = atoi(argv[i]);
+                if (force_size < 1 || force_size > 6)
+                {
+                    cout << "Force Size must be between 1 and 6" << endl;
+                    exit(1);
+                }
+            }
+            else
+            {
+                print_usage();
+                exit(1);
+            }
+        }
+        else if (strcmp(argv[i], "--numForces") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                i++;
+                num_model_forces = atoi(argv[i]);
+                if (num_model_forces < 1 || num_model_forces > 5)
+                {
+                    cout << "Num Forces must be between 1 and 5" << endl;
+                    exit(1);
+                }
+            }
+            else
+            {
+                print_usage();
+                exit(1);
+            }
+        }
+        else if (strcmp(argv[i], "--underPercentage") == 0)
+        {
+            if (i + 1 < argc)
+            {
+                i++;
+                under_percentage = atof(argv[i]);
+                if (under_percentage < 0.0f || under_percentage > 1.0f)
+                {
+                    cout << "Under percentage must be between 0.0 and 1.0" << endl;
+                    exit(1);
+                }
+            }
+            else
+            {
+                print_usage();
+                exit(1);
+            }
+        }
+        else
+        {
+            cout << "Unrecognized option: " << argv[i] << endl << endl;
+            print_usage();
+            exit(1);
+        }
+    }
+
+    if (max_bv < 0)
+    {
+        max_bv = 5000;
+    }
+    
+    if (force_size < 0)
+    {
+        force_size = 4;
+    }
+    
+    if (under_percentage < 0.0)
+    {
+        under_percentage = 0.95f;
+    }
+    
+    if (num_model_forces < 0)
+    {
+        num_model_forces = 3;
+    }
+    
     int max_attempts = 5000;
     set<Force> force_set;
     auto models = &is_models;
