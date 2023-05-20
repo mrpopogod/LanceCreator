8bc246859acddcb133c2c8a27edffe1d4701e4cf
Aaron Doukas <amyleeslave@gmail.com>
2023-05-20 08:30:21 -0700

    Beginning conversion to rust

---
+++ src/model/mod.rs
@@ -0,0 +1,62 @@
+use sorted_vec::SortedVec;
+
+#[derive(PartialEq, PartialOrd, Eq, Ord)]
+pub struct Mech {
+    pub name: String,
+    pub variant: String,
+    pub bv: u32,
+}
+
+#[derive(PartialEq, PartialOrd)]
+pub struct Model {
+    pub name: String,
+    pub count: u8,
+    pub variants: SortedVec<Mech>,
+}
+
+impl Model {
+    pub fn min_bv(&self) -> u32 {
+        let mut min = u32::MAX;
+        for mech in self.variants.iter() {
+            if mech.bv < min {
+                min = mech.bv;
+            }
+        }
+
+        min
+    }
+
+    pub fn max_bv(&self) -> u32 {
+        let mut max = u32::MIN;
+        for mech in self.variants.iter() {
+            if mech.bv > max {
+                max = mech.bv;
+            }
+        }
+
+        max
+    }
+}
+
+#[derive(PartialEq, PartialOrd)]
+pub struct Force {
+    pub mechs: SortedVec<Mech>,
+    pub bv: u32,
+}
+
+impl Force {
+    pub fn to_csv(&self) -> String {
+        let mut retval = String::new();
+        let mut bv = 0;
+        for mech in self.mechs.iter() {
+            bv += mech.bv;
+            retval += &mech.name;
+            retval += " ";
+            retval += &mech.variant;
+            retval += ",";
+        }
+
+        retval += &bv.to_string();
+        retval
+    }
+}
